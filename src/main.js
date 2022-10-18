7f90d7e87a8f2dd4716d69985a4538588eb778ef
Vitor Rocha <65897710+Vitorrocha98@users.noreply.github.com>
2022-10-18 02:03:19 -0300

    tornando a função global

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
-function setCarType(type){
+function setCardType(type){
   const colors = {
     visa: ["#436D99", "#2D57F2"],
     mastercard: ["#DF6F29", "#C69347"],
@@ -16,4 +16,5 @@ function setCarType(type){
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-setCarType("mastercard")
+//tornando a funcao global
+globalThis.setCardType = setCardType
