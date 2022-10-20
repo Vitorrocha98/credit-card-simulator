0eaa87c0c42bb76ad43dd32089feb3bf449dbfe4
Vitor Rocha <65897710+Vitorrocha98@users.noreply.github.com>
2022-10-20 02:35:13 -0300

    criando mascaras para codigo de segurança e data

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
-function setCardType(type){
+function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
     mastercard: ["#DF6F29", "#C69347"],
@@ -18,3 +19,31 @@ function setCardType(type){
 
 //tornando a funcao global
 globalThis.setCardType = setCardType
+
+//security code
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+// expirationDate
+// nesse campo o valor do mes vai de 1 a 10, com dois digitos
+// e o valor do ano vai ser do ano atual ate 10 anos a frente, com dois digitos
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+}
+const expirationDateMasked = IMask(expirationDate, securityCodePattern)
